798f619daa3e6f68fcd1040618b319b2d139f286
Danial Nayyar (MEng, Hons) <110974901+DanialNayyar@users.noreply.github.com>
2026-08-03 18:15:05 +0100

    Add: squares-of-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        #loop through nums
+        # method 1: square i then sort
+        # method 2: use two pointer to sort instead of .sort()
+                # left and right pointers, 
+                    # left at the start,
+                    # right = left +=1
+                    # if left < right
+# method 1
+        for i in range(0, len(nums)):
+            nums[i] = nums[i]**2
+        
+        nums.sort()
+        return nums
+
+            
+            
